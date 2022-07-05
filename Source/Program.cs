13ea46c01e18d6bc4f77bfc8145f92e297c6a17f
NathanKell <nathankell@gmail.com>
2022-07-04 23:42:29 -0700

    Now can take root dir as argument (pass it a repo root). Will print help if fails.

--- Source/Program.cs
+++ Source/Program.cs
@@ -11,16 +11,37 @@ namespace CFGProjectGenerator
         {
             if (args.Length != 1)
             {
-                Console.WriteLine($"Error: No file specified. Gave {args.Length} arguments.");
-                foreach (string a in args)
-                    Console.WriteLine(a);
+                Console.WriteLine($"Error: No path or file specified.");
+                PrintHelp();
                 return 1;
             }
+            if (args[0] == "-h" || args[0] == "--help")
+            {
+                PrintHelp();
+                return 0;
+            }
             string szFilepath = args[0];
-
-            string szBuf = File.ReadAllText(szFilepath);
-
             string szAbsPath = Path.GetFullPath(szFilepath);
+            if (Directory.Exists(szAbsPath) && !File.Exists(szAbsPath))
+            {
+                // Find a "GameData.csproj.in" in some subdirectory
+                string szTemp = PathToProject(new DirectoryInfo(szAbsPath));
+                if (string.IsNullOrEmpty(szTemp))
+                {
+                    Console.WriteLine($"Could not find GameData.csproj.in under directory {szAbsPath}!");
+                    return 1;
+                }
+                szAbsPath = szTemp;
+            }
+            if (!File.Exists(szAbsPath))
+            {
+                Console.WriteLine($"File {szAbsPath} does not exist!");
+                return 1;
+            }
+
+            string szBuf = File.ReadAllText(szAbsPath);
+
+            
             string szRelPath = string.Empty;
             DirectoryInfo dirInfo = new DirectoryInfo(Path.GetDirectoryName(szAbsPath));
             while (dirInfo.Exists && dirInfo.Parent != null && dirInfo.GetDirectories().FirstOrDefault(d => d.Name == "GameData") == null)
@@ -61,6 +82,45 @@ namespace CFGProjectGenerator
             return 0;
         }
 
+        /// <summary>
+        /// Prints help text
+        /// </summary>
+        static void PrintHelp()
+        {
+            Console.WriteLine("Usage: `CFGProjectGenerator.exe path/to/inputfile.csproj.in`");
+            Console.WriteLine("Or: `CFGProjectGenerator.exe rootpath`\n\t(under which there is a GameData.csproj.in in that or some subdir)");
+            Console.WriteLine("In the .csproj.in file, the string `<<content>>` will be replaces with all cfg files found in GameData");
+        }
+
+        /// <summary>
+        /// Finds "GameData.csproj.in" in some subdir
+        /// and returns the full path
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <returns></returns>
+        static string PathToProject(DirectoryInfo dir)
+        {
+            FileInfo fi = dir.GetFiles().FirstOrDefault(f => f.Name == "GameData.csproj.in");
+            if (fi != null)
+                return fi.FullName;
+
+            foreach (DirectoryInfo di in dir.GetDirectories())
+            {
+                string szPath = PathToProject(di);
+                if (!string.IsNullOrEmpty(szPath))
+                    return szPath;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Recursively build a list of all cfg files in subdirectories
+        /// </summary>
+        /// <param name="curDir">Directory to search</param>
+        /// <param name="szPath">current path to display</param>
+        /// <param name="szBasePath">file system path prepended to szPath</param>
+        /// <returns></returns>
         static string BuildFileList(DirectoryInfo curDir, string szPath, string szBasePath)
         {
             string szFiles = string.Empty;
